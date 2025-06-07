13b709c45922f234d4fd28e22afe6c717a848ad1
Everett Van Buskirk <everettvanbuskirk@gmail.com>
2025-06-06 19:50:33 -0700

    push fixes

--- backend/src/routes/playlist.ts
+++
@@ -1,9 +0,0 @@
-// backend/src/routes/playlist.ts
-import { Router } from 'express';
-import { createPlaylist } from '../controllers/playlistController';
-
-const router = Router();
-
-router.post('/create', createPlaylist);
-
-export default router;
--- backend/src/routes/setlist.ts
+++
@@ -1,9 +0,0 @@
-// backend/src/routes/setlist.ts
-import { Router } from 'express';
-import { searchSetlists } from '../controllers/setlistController';
-
-const router = Router();
-
-router.get('/search', searchSetlists);
-
-export default router;
